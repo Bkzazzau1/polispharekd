4268b9badb942cec04684260454678abc897af61
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-12 16:54:22 +0100

    Add Kaduna geography baseline

---
+++ lib/kaduna/geography.dart
@@ -0,0 +1,35 @@
+class KadunaGeography {
+  const KadunaGeography._();
+
+  static const lgas = <String>[
+    'Birnin Gwari',
+    'Chikun',
+    'Giwa',
+    'Igabi',
+    'Ikara',
+    'Jaba',
+    "Jema'a",
+    'Kachia',
+    'Kaduna North',
+    'Kaduna South',
+    'Kagarko',
+    'Kajuru',
+    'Kaura',
+    'Kauru',
+    'Kubau',
+    'Kudan',
+    'Lere',
+    'Makarfi',
+    'Sabon Gari',
+    'Sanga',
+    'Soba',
+    'Zangon Kataf',
+    'Zaria',
+  ];
+
+  static const lgaCount = 23;
+  static const wardCount = 255;
+  static const pollingUnitCount = 8012;
+  static const registeredVoters2023 = 4335208;
+  static const pvcCollected2023 = 4164473;
+}
