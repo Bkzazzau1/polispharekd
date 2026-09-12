bd39ef3259621d4dff7e157cf87bfdcea64887de
Bkzazzau1 <y.i.bashir@gmail.com>
2026-09-12 16:54:12 +0100

    Add Kaduna campaign identity

---
+++ lib/kaduna/campaign_identity.dart
@@ -0,0 +1,9 @@
+class KadunaIdentity {
+  const KadunaIdentity._();
+
+  static const productName = 'POLISPHERE KADUNA';
+  static const principalName = 'Senator Uba Sani, CON';
+  static const principalTitle = 'Governor of Kaduna State';
+  static const commandTitle = 'Governor Uba Sani Command Centre';
+  static const stateName = 'Kaduna State';
+}
